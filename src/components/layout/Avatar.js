63caeb17f5a6db414d8591286cf2157f81d1f96d
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-04-09 17:24:19 +0200

    Add Avatar component

---
+++ src/components/layout/Avatar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import WebsiteIcon from 'components/WebsiteIcon'
+
+const Figure = styled.figure`
+	margin: 0;
+	padding: 0;
+	box-shadow: 0 4px 7px rgba(0, 0, 0, 0.23), 0 8px 25px rgba(0, 0, 0, 0.05);
+	padding: 5px;
+`
+
+const Image = styled.img`
+	max-width: 100%;
+`
+
+const Name = styled.p`
+	font-size: 1.2rem;
+`
+
+const Avatar = ({ name, image, website, description }) => {
+	return (
+		<Figure>
+			<Image src={`/avatars/${image}`} />
+			<Name>
+				<strong>{name}</strong>
+				{website && <WebsiteIcon url={website} />}
+			</Name>
+			<p>{description}</p>
+		</Figure>
+	)
+}
+
+Avatar.propTypes = {
+	name: PropTypes.string.isRequired,
+	image: PropTypes.string.isRequired,
+	website: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+	description: PropTypes.string.isRequired,
+}
+
+export default Avatar
