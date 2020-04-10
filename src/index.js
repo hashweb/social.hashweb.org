c2459e5e849a171c7929aca3e37bc09849d86176
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-04-10 04:01:37 +0200

    Add fontawesome icons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import { render } from 'react-dom'
 import { Helmet } from 'react-helmet'
 import { ThemeProvider } from 'styled-components'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCrown, faLink } from '@fortawesome/pro-duotone-svg-icons'
+// import { fal } from '@fortawesome/pro-light-svg-icons'
+// import { far } from '@fortawesome/pro-regular-svg-icons'
+// import { fas } from '@fortawesome/pro-solid-svg-icons'
+
+library.add(faCrown, faLink)
+
 import { defaultTheme, GlobalStyle } from 'styles'
 import App from 'components/App'
 
