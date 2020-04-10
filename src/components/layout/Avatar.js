41524cb7d3d9b07d506d32a774f74ef701a78460
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-04-10 04:05:09 +0200

    Remove WebsiteIcon and use FontAwesome icon instead

--- src/components/layout/Avatar.js
+++ src/components/layout/Avatar.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-
-import WebsiteIcon from 'components/WebsiteIcon'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Figure = styled.figure`
 	margin: 0;
@@ -19,13 +18,25 @@ const Name = styled.p`
 	font-size: 1.2rem;
 `
 
+const WebsiteIcon = styled(FontAwesomeIcon)`
+	color: #673ab7;
+
+	&:hover {
+		cursor: pointer;
+	}
+`
+
 const Avatar = ({ name, image, website, description }) => {
+	const openWebsite = () =>
+		window.open(website, '_blank', 'noopener noreferrer')
 	return (
 		<Figure>
 			<Image src={`/avatars/${image}`} alt={name} />
 			<Name>
 				<strong>{name}</strong>
-				{website && <WebsiteIcon url={website} />}
+				{website && (
+					<WebsiteIcon icon={['fad', 'link']} onClick={openWebsite} />
+				)}
 			</Name>
 			<p>{description}</p>
 		</Figure>
