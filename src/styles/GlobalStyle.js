b287a8c415263f15255a8eb1eb6c72aa5f38396e
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-04-11 01:36:01 +0200

    Remove styled-normalize

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
-import { normalize } from 'styled-normalize'
 
 export const GlobalStyle = createGlobalStyle`
-	${normalize}
-
 	* {
 		margin: 0;
 		padding: 0;
