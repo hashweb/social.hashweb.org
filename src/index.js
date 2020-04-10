3a26ddfabe04fe4a2c91447eb6ff3d7a117fc840
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-04-10 04:07:22 +0200

    Move Google font stylesheet to template

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Helmet } from 'react-helmet'
 import { ThemeProvider } from 'styled-components'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -17,12 +16,6 @@ import App from 'components/App'
 render(
 	<ThemeProvider theme={defaultTheme}>
 		<GlobalStyle />
-		<Helmet>
-			<link
-				href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;700&display=swap"
-				rel="stylesheet"
-			/>
-		</Helmet>
 		<App />
 	</ThemeProvider>,
 	document.getElementById('root'),
