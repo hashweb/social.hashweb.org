2e301cda6df07eefb5c7f149379e7229f4644f2a
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-04-15 18:51:59 +0200

    Use babel.config.js instead of webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -55,14 +55,9 @@ export default (env, argv) => {
 				{
 					test: /\.(js|jsx)$/,
 					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: [
-								'@babel/preset-env',
-								'@babel/preset-react',
-							],
-						},
+					loader: 'babel-loader',
+					options: {
+						envName: argv.mode,
 					},
 				},
 				{
