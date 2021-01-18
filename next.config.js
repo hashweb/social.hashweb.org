adef477c4ea4c411ac9d4f51d95fab928519c2c6
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2021-01-18 23:50:17 +0100

    Set custom cache control of avatars URLs

--- next.config.js
+++ next.config.js
@@ -7,6 +7,22 @@ module.exports = {
 	reactStrictMode: false,
 	poweredByHeader: false,
 	assetPrefix,
+	async headers() {
+		return [
+			{
+				source: '/avatars/:path*',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 'public, max-age=15552000',
+					},
+				],
+			},
+		]
+	},
+	images: {
+		domains: [assetPrefix],
+	},
 	serverRuntimeConfig: {},
 	publicRuntimeConfig: {
 		assetPrefix,
