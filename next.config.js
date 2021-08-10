2ad5c37e6684e012f244e5162e416772c2738802
Ole-Martin Bratteng <ole-martin@bratteng.com>
2021-08-10 10:38:53 +0200

    build: add webpack bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPlugins = require('next-compose-plugins')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+})
+
 const isProd = process.env.NODE_ENV === 'production' && process.env.APP_ENV !== 'staging'
 
 const CDN = 'https://socialhashweb.b-cdn.net'
 const assetPrefix = isProd ? CDN : ''
 
-module.exports = {
+const nextConfig = {
 	reactStrictMode: false,
 	poweredByHeader: false,
 	assetPrefix,
@@ -28,3 +34,5 @@ module.exports = {
 		assetPrefix,
 	},
 }
+
+module.exports = withPlugins([withBundleAnalyzer], nextConfig)
